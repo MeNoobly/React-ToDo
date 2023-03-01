540441c23e7fd67af5de2302e9bad137e22c2e26
MeNoobly <84956743974@mail.ru>
2023-03-01 23:34:21 +0300

    add edit, change some style

--- src/components/Action.jsx
+++ src/components/Action.jsx
@@ -1,12 +1,11 @@
 ﻿import React, { useState } from 'react';
 import button from './UI/Button/Button.module.css';
-import input from "../components/UI/Input/Input.module.css";
 import styles from "../styles/modules/Action.module.css";
 import Button from './UI/Button/Button';
-import Input from './UI/Input/Input';
+import FormEditAction from './UI/Forms/FormEditAction/FormEditAction';
 
 const Action = (props) => {
-    const [text, setText] = useState(`${props.number + 1}. ${props.action.body}`);
+    const [text, setText] = useState(`${props.action.body}`);
     const [isEdit, setIsEdit] = useState(false);
 
     if (!isEdit) {
@@ -25,19 +24,7 @@ const Action = (props) => {
     }
 
     return (
-        <div className={styles.ActionBlock}>
-            <Input 
-                value={text}
-                onChange={(event) => setText(event.target.value)}
-                onBlur={() => setIsEdit(false)}
-                className={`${input.Input} ${styles.ActionEditInput}`}
-            />
-            <Button 
-                className={`${button.Button} ${styles.ActionEditButton}`}
-                text={"Сохранить"}
-                onClick={() => setIsEdit(false)}
-            />
-        </div>
+        <FormEditAction text={text} setText={setText} setIsEdit={setIsEdit}/>
     );
 };
 
